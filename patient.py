7665896189c1039663d49d757626693b0845b66b
Celeste Wilkins <celestewilkins@Celestes-MacBook-Air.local>
2021-06-29 11:43:44 -0300

    Coding style improvements

--- patient.py
+++ patient.py
@@ -13,17 +13,22 @@ def main(args):
     - selecting the necessary models and views for the current task
     - passing data between models and views
     """
-    InFiles = args.infiles
-    if not isinstance(InFiles, list):
-        InFiles = [args.infiles]
+
+    infiles = args.infiles
+    if not isinstance(infiles, list):
 
 
-    for filename in InFiles:
-        inflammation_data = models.load_csv(filename)
+infiles = [args.infiles]
 
-        view_data = {'average': models.daily_mean(inflammation_data), 'max': models.daily_max(inflammation_data), 'min': models.daily_min(inflammation_data)}
+for filename in infiles:
+    inflammation_data = models.load_csv(filename)
 
-        views.visualize(view_data)
+    view_data = {'average': models.daily_mean(inflammation_data),
+                 'max': models.daily_max(inflammation_data),
+                 'min': models.daily_min(inflammation_data)
+                 }
+
+    views.visualize(view_data)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
